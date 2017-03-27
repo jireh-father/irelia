b9defe94e20e76ceeddd07caabbbc214af0d005b
서일근 <seoilgun@gmail.com>
2017-03-28 08:45:00 +0900

    irelia v2

--- util/neural_network.py
+++ util/neural_network.py
@@ -4,12 +4,22 @@ num_output_filter = 2
 dtype = tf.float16
 
 
+def decode_shape(tensor, data_format=None):
+    input_shape = list(tensor.get_shape())
+    if data_format is 'NCHW':
+        height = input_shape[2]
+        width = input_shape[3]
+        num_input_feature = input_shape[1]
+    else:
+        height = input_shape[1]
+        width = input_shape[2]
+        num_input_feature = input_shape[3]
+    return height, width, num_input_feature
+
+
 def sl_policy_network(inputs=None, num_repeat_layers=11, num_filters=192,
-                      filter_initializer=tf.contrib.layers.xavier_initializer()):
-    input_shape = list(inputs.get_shape())
-    height = input_shape[1]
-    width = input_shape[2]
-    num_input_feature = input_shape[3]
+                      filter_initializer=tf.contrib.layers.xavier_initializer(), data_format=None):
+    height, width, num_input_feature = decode_shape(inputs, data_format)
     end_points = {}
 
     # layer 1
@@ -17,7 +27,7 @@ def sl_policy_network(inputs=None, num_repeat_layers=11, num_filters=192,
         filters = tf.get_variable("filter1", shape=[5, 5, num_input_feature, num_filters], dtype=dtype,
                                   initializer=filter_initializer)
 
-        net = tf.nn.conv2d(inputs, filters, strides=[1, 1, 1, 1], padding='SAME')
+        net = tf.nn.conv2d(inputs, filters, strides=[1, 1, 1, 1], padding='SAME', data_format=data_format)
         net = tf.nn.relu(net)
         end_points['conv1'] = net
 
@@ -28,7 +38,7 @@ def sl_policy_network(inputs=None, num_repeat_layers=11, num_filters=192,
             filters = tf.get_variable("filter" + str(layer_no), shape=[3, 3, num_filters, num_filters], dtype=dtype,
                                       initializer=filter_initializer)
 
-            net = tf.nn.conv2d(net, filters, strides=[1, 1, 1, 1], padding='SAME')
+            net = tf.nn.conv2d(net, filters, strides=[1, 1, 1, 1], padding='SAME', data_format=data_format)
             net = tf.nn.relu(net)
             end_points['conv' + str(layer_no)] = net
 
@@ -36,9 +46,15 @@ def sl_policy_network(inputs=None, num_repeat_layers=11, num_filters=192,
     with tf.variable_scope("conv13"):
         filters = tf.get_variable("filter13", shape=[1, 1, num_filters, num_output_filter], dtype=dtype,
                                   initializer=filter_initializer)
-        biases = tf.get_variable("biases", [height, width, num_output_filter], initializer=tf.constant_initializer(0.0),
-                                 dtype=dtype)
-        net = tf.nn.conv2d(net, filters, strides=[1, 1, 1, 1], padding='SAME')
+        if data_format is 'NCHW':
+            biases = tf.get_variable("biases", [num_output_filter, height, width],
+                                     initializer=tf.constant_initializer(0.0),
+                                     dtype=dtype)
+        else:
+            biases = tf.get_variable("biases", [height, width, num_output_filter],
+                                     initializer=tf.constant_initializer(0.0),
+                                     dtype=dtype)
+        net = tf.nn.conv2d(net, filters, strides=[1, 1, 1, 1], padding='SAME', data_format=data_format)
         logits = net + biases
         end_points['Logits'] = logits
         end_points['Predictions'] = tf.nn.softmax(logits, name='Predictions')
