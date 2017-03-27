1c04570f4f4de29b6c65ca541c22d99806ec5a78
서일근 <seoilgun@gmail.com>
2017-03-27 16:11:20 +0900

    irelia v2

--- train_sl_policy_network.py
+++ train_sl_policy_network.py
@@ -6,6 +6,8 @@ FLAGS = tf.app.flags.FLAGS
 tf.app.flags.DEFINE_integer('batch_size', 16, 'The number of samples in each batch.')
 tf.app.flags.DEFINE_integer('num_filters', 192, 'The number of cnn filters.')
 tf.app.flags.DEFINE_integer('num_repeat_layers', 11, 'The number of cnn repeat layers.')
+tf.app.flags.DEFINE_integer('max_number_of_steps', 100, 'The maximum number of training steps.')
+tf.app.flags.DEFINE_float('learning_rate', 0.01, 'learning_rate.')
 
 width = 9
 height = 10
@@ -23,7 +25,7 @@ with tf.variable_scope('cross_entropy'):
 
 # train
 with tf.name_scope('train'):
-    optimizer = tf.train.RMSPropOptimizer(0.1, decay=0.9, momentum=0.9, epsilon=1.0)
+    optimizer = tf.train.RMSPropOptimizer(FLAGS.learning_rate, decay=0.9, momentum=0.9, epsilon=1.0)
     # train = tf.train.MomentumOptimizer(learning_rate, momentum).minimize(cost)
     train = optimizer.minimize(loss)
 
@@ -53,6 +55,6 @@ y_train = [[[[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0,
             [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]],
             [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]]]]
 
-for i in range(1000):
+for i in range(FLAGS.max_number_of_steps):
     curr_loss, curr_logits, _ = sess.run([loss, logits, train], {inputs: x_train, labels: y_train})
     print("loss: %s %s" % (curr_loss, str(curr_logits)))
