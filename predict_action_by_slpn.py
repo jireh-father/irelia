5bdc41832554586e6f33b2a905c2b7c3240dd691
서일근 <seoilgun@gmail.com>
2017-04-06 17:56:04 +0900

    irelia v2

--- predict_action_by_slpn.py
+++ predict_action_by_slpn.py
@@ -13,7 +13,7 @@ tf.app.flags.DEFINE_integer('num_filters', 192, 'The number of cnn filters.')
 tf.app.flags.DEFINE_string('data_format', 'NCHW', 'cnn data format')
 tf.app.flags.DEFINE_string('checkpoint_path', '/home/igseo/data/korean_chess/train_log/sl_policy_network.ckpt',
                            'cnn data format')
-tf.app.flags.DEFINE_string('state',
+tf.app.flags.DEFINE_string('state_key',
                            'r6,r4,r2,r3,1,r3,r2,r4,r6,4,r7,5,r5,5,r5,1,r1,1,r1,1,r1,1,r1,1,r1,18,b1,1,b1,1,b1,1,b1,1,b1,1,b5,5,b5,5,b7,4,b6,b2,b4,b3,1,b3,b4,b2,b6',
                            'current state')
 
@@ -40,31 +40,31 @@ sess.run(init)
 saver = tf.train.Saver()
 saver.restore(sess, F.checkpoint_path)
 
-x_train = game.convert_state_feature_map(F.state, F.color)
+x_train = game.convert_state_feature_map(F.state_key, F.color)
 if F.data_format is not 'NCHW':
     x_train = np.transpose(x_train, (0, 2, 3, 1))
 result, pred = sess.run([argmax, end_points['Predictions']], {inputs: x_train})
 
-before_list = np.argsort(-pred[0][0])
-after_list = np.argsort(-pred[0][1])
-print(before_list)
-print(after_list)
-print(pred[0][0][before_list])
-print(pred[0][1][after_list])
-actions_dict = game.get_actions_hash_map(F.state, F.color)
+from_list = np.argsort(-pred[0][0])
+to_list = np.argsort(-pred[0][1])
+print(from_list)
+print(to_list)
+print(pred[0][0][from_list])
+print(pred[0][1][to_list])
+actions_dict = game.get_actions_hash_map(F.state_key, F.color)
 result_dict = {}
-for after_position in after_list:
+for after_position in to_list:
     to_value = pred[0][1][after_position]
     to_x, to_y = game.convert_one_dim_pos_to_two_dim_pos(after_position)
-    for before_position in before_list:
-        if before_list[before_position] is False:
+    for from_position in from_list:
+        if from_list[from_position] is False:
             continue
-        before_value = pred[0][0][before_position]
-        x, y = game.convert_one_dim_pos_to_two_dim_pos(before_position)
+        from_value = pred[0][0][from_position]
+        x, y = game.convert_one_dim_pos_to_two_dim_pos(from_position)
         pos_key = game.build_pos_key(x, y, to_x, to_y)
         if pos_key in actions_dict:
-            result_dict[pos_key] = before_value + to_value
-            before_list[before_position] = False
+            result_dict[pos_key] = (from_value + to_value) / 2
+            from_list[from_position] = False
             break
 
 result_dict = sorted(result_dict.items(), key=operator.itemgetter(1))
@@ -79,5 +79,7 @@ probabilities = e / np.sum(e)
 
 sample = np.random.choice(key_list, 1, p=probabilities)
 print(value_list)
+print(np.sum(np.array(value_list)))
 print(probabilities)
+print(np.sum(probabilities))
 print(sample[0])
