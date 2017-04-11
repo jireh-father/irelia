fac33b4269518dfd6efb8d13bd4db7fc962fa168
서일근 <seoilgun@gmail.com>
2017-04-11 17:09:34 +0900

    irelia v2

--- action_api.py
+++ action_api.py
@@ -2,11 +2,9 @@ from env.korean_chess.core import Core
 from flask import Flask, render_template
 from flask import request
 import json
-import sqlite3
-import operator
-import random
-import numpy as np
-from elasticsearch import Elasticsearch as ES
+import requests
+
+from util import sl_policy_network
 
 app = Flask(__name__)
 
@@ -32,40 +30,14 @@ def action():
         return json.dumps({'error': True, 'msg': 'invalid params', 'data': {'state_map': state_map, 'side': side}})
 
     state_map = json.loads(state_map)
-    filter_state_map(state_map)
-    if side == 'b':
-        reverse_state_map = Core.reverse_state_map(state_map)
-        db_name = './q_blue.db'
-    else:
-        reverse_state_map = state_map
-        db_name = './q_red.db'
-
-    state_key = Core.convert_state_key(reverse_state_map)
-
-    conn = sqlite3.connect(db_name)
-
-    c = conn.cursor()
-
-    c.execute("SELECT quality_json FROM t_quality WHERE state_key='" + state_key + "'")
-
-    result = c.fetchone()
-    reversed_state_map = Core.reverse_state_map(state_map)
-    actions = Core.get_actions(reversed_state_map, side)
-    if result:
-        if result[0] == '0':
-            action = Core.similar_action(actions, state_key, side, c)
-        else:
-            q_values = json.loads(result[0])
-            max_action = int(max(q_values.iteritems(), key=operator.itemgetter(1))[0])
-            if len(actions) <= max_action:
-                action = Core.similar_action(actions, state_key, side, c)
-            else:
-                action = actions[max_action]
-    else:
-        action = Core.similar_action(actions, state_key, side, c)
-
-    c.close()
-    return json.dumps(action)
+    state_key = Core.convert_state_key(state_map)
+    response = requests.get('ec2-52-79-229-138.ap-northeast-2.compute.amazonaws.com',
+                            {'state_key': state_key, 'color': side},
+                            timeout=12)
+    if response.status_code != 200:
+        return json.dumps(
+            {'error': True, 'msg': 'another server request error', 'response': json.loads(response.json())})
+    return response.json()
 
 
 @app.route("/actions")
@@ -83,6 +55,30 @@ def actions():
 
 def error(msg, data=None):
     return json.dumps({'error': True, 'msg': msg, 'data': data})
+
+
+@app.route("/action_by_slpn")
+def action_by_slpn():
+    state_key = request.args.get('state_key')
+    color = request.args.get('color')
+
+    retry_cnt = 3
+    for i in range(retry_cnt):
+        try:
+            action = sl_policy_network.sampling_action(state_key, color,
+                                                       '/home/model/korean_chess/sl_policy_network.ckpt')
+            break
+        except Exception as e:
+            1
+    if not action:
+        json.dumps({'error': True})
+    else:
+        action_list = action.split('_')
+
+        json.dumps({'x': action_list[0],
+                    'y': action_list[1],
+                    'to_x': action_list[2],
+                    'to_y': action_list[3]})
 
 
 if __name__ == "__main__":
---
+++ util/sl_policy_network.py
@@ -0,0 +1,59 @@
+import tensorflow as tf
+from util import neural_network as nn
+from core import game
+import numpy as np
+import operator
+
+
+def sampling_action(state_key, color, checkpoint_path):
+    width = 9
+    height = 10
+    num_input_feature = 3
+    data_format = 'NCHW'
+
+    inputs = tf.placeholder(tf.float16, [None, num_input_feature, height, width], name='inputs')
+
+    logits, end_points = nn.sl_policy_network(inputs, 11, 192, data_format=data_format)
+
+    argmax = tf.argmax(end_points['Predictions'], 2)
+
+    init = tf.global_variables_initializer()
+    sess = tf.InteractiveSession()
+    sess.run(init)
+
+    saver = tf.train.Saver()
+    saver.restore(sess, checkpoint_path)
+
+    x_train = game.convert_state_feature_map(state_key, color)
+    result, pred = sess.run([argmax, end_points['Predictions']], {inputs: x_train})
+
+    from_list = np.argsort(-pred[0][0])
+    to_list = np.argsort(-pred[0][1])
+    actions_dict = game.get_actions_hash_map(state_key, color)
+    result_dict = {}
+    for after_position in to_list:
+        to_value = pred[0][1][after_position]
+        to_x, to_y = game.convert_one_dim_pos_to_two_dim_pos(after_position)
+        for from_position in from_list:
+            if from_list[from_position] is False:
+                continue
+            from_value = pred[0][0][from_position]
+            x, y = game.convert_one_dim_pos_to_two_dim_pos(from_position)
+            pos_key = game.build_pos_key(x, y, to_x, to_y)
+            if pos_key in actions_dict:
+                result_dict[pos_key] = (from_value + to_value) / 2
+                from_list[from_position] = False
+                break
+    if len(result_dict) < 1:
+        return False
+    result_dict = sorted(result_dict.items(), key=operator.itemgetter(1))
+    result_dict.reverse()
+    key_list = [x[0] for x in result_dict]
+    value_list = [x[1] for x in result_dict]
+
+    np_value_list = np.array(value_list)
+    sum = np.sum(np_value_list)
+    probabilities = np_value_list / sum
+
+    sample = np.random.choice(key_list, 1, p=probabilities)
+    return sample[0]
