ae6af2248564187681e51ede6fc9c9c5e7db8bfc
서일근 <seoilgun@gmail.com>
2017-04-11 17:47:15 +0900

    irelia v2

--- action_api.py
+++ action_api.py
@@ -1,3 +1,7 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 from env.korean_chess.core import Core
 from flask import Flask, render_template
 from flask import request
