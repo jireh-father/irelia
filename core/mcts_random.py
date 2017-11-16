1fbd770864c568df42dee00071c43ce898461720
seoilgun <seoilgun@gmail.com>
2017-11-17 00:36:21 +0900

    under construction

--- core/mcts_random.py
+++ core/mcts_random.py
@@ -1,13 +1,12 @@
 # coding=utf8
 import math
 import numpy as np
+import random
 
 
 class Mcts(object):
-    def __init__(self, state, env, model, max_simulation=500, winner_reward=1, loser_reward=-1, use_best=True,
-                 c_puct=0.5):
+    def __init__(self, state, env, max_simulation=500, winner_reward=1, loser_reward=-1, use_best=True, c_puct=0.5):
         self.env = env
-        self.model = model
         self.max_simulation = max_simulation
         self.root_node = Node(state)
         self.selected_edges = []
@@ -82,41 +81,33 @@ class Mcts(object):
         print("Expand and Evaluate!")
         if self.env.is_over(self.current_node.state):
             print("MCTS Game Over")
-            return self.loser_reward
+            return self.loser_rewardf
 
-        # todo: !!!<일단 요것만구현>반복수 제한도 걸기(여러 반복수하면 비겨서 계산하는 조건도 알아보기)
-        # todo :pass액션 추가 ( 둘다 pass할경우 점수계산으로
-        action_probs, state_value = self.model.inference(self.current_node.state)
-        print("MCTS Value inference", state_value)
-        # todo : <<빅장>> 혹은 외통수(장군)등 기능 구현?
-        # todo: 비긴 상태 구현해서 적용하기(더 디테일하게)
+        state_value = self.rollout(self.current_node.state)
 
         legal_actions = self.env.get_all_actions(self.current_node.state)
 
         if not legal_actions:
             return self.loser_reward
 
-        legal_action_probs = []
-        for legal_action in legal_actions:
-            legal_action = self.env.encode_action(legal_action)
-            legal_action_probs.append(action_probs[legal_action[0]] + action_probs[legal_action[0]])
-
-        legal_action_probs = np.array(legal_action_probs)
-        # todo: add noise!! check (DIR(0.03)???)
-        if self.root_node is self.current_node:
-            # add noise to prior probabilities
-            if (legal_action_probs == 0).all():
-                noise_probs = legal_action_probs
-            else:
-                noise_probs = np.random.dirichlet(legal_action_probs, 1)[0]
-
-            legal_action_probs = ((1 - 0.25) * legal_action_probs + (noise_probs * 0.25))
-
         self.current_node.edges = [
-            Edge(action_prob, self.env.simulate(self.current_node.state, legal_actions[i]), legal_actions[i]) for
-            i, action_prob in enumerate(legal_action_probs)]
+            Edge(self.env.simulate(self.current_node.state, action), action) for
+            i, action in enumerate(legal_actions)]
 
         return state_value
+
+    def rollout(self, state):
+        i = 0
+        while True:
+            legal_actions = self.env.get_all_actions(self.current_node.state)
+            action = legal_actions[random.randint(0, len(legal_actions) - 1)]
+            state = self.env.simulate(state, action)
+            if self.env.is_over(state):
+                if i % 2 == 0:
+                    return self.winner_reward
+                else:
+                    return self.loser_reward
+            i += 1
 
     def backup(self, state_value):
         print("MCTS Backup")
@@ -137,7 +128,7 @@ class Node(object):
 
 
 class Edge(object):
-    def __init__(self, action_prob, state, action):
+    def __init__(self, state, action):
         # N
         self.visit_count = 0
         # W
@@ -145,7 +136,6 @@ class Edge(object):
         # Q
         self.mean_action_value = 0
         # P
-        self.action_prob = action_prob
         self.action = action
         self.node = Node(state)
 
@@ -159,7 +149,7 @@ class Edge(object):
         total_other_edge_visit_count = 0
         for edge in edges:
             total_other_edge_visit_count += edge.visit_count
-        U = c_puct * self.action_prob * (math.sqrt(total_other_edge_visit_count) / (1 + self.visit_count))
+        U = c_puct * (math.sqrt(total_other_edge_visit_count) / (1 + self.visit_count))
         return self.mean_action_value + U
 
     def get_action_probs(self, edges, temperature):
