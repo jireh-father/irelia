82534a093761736d57e332099d02b2abfc08f72e
seoilgun <seoilgun@gmail.com>
2017-12-03 06:58:56 +0900

    under construction

--- parse_dataset.py
+++ parse_dataset.py
@@ -25,19 +25,19 @@ for n, line in enumerate(f):
     first_state = env.reset()
     info = {"over_limit_step": False, "is_draw": False, "winner": winner}
     state, _ = env.encode_state(first_state)
-    state_history = [first_state]
+    state_history = [first_state.tolist()]
     mcts_history = []
     for i, action in enumerate(actions):
         turn = "r" if i % 2 == 0 else "b"
         state[action["to_y"]][action["to_x"]] = state[action["y"]][action["x"]]
         state[action["y"]][action["x"]] = 0
-        state_history.append(env.decode_state(state, turn))
+        state_history.append(env.decode_state(state, turn).tolist())
         policy_probs = np.array([.0] * 90)
         action["from_x"] = action["x"]
         action["from_y"] = action["y"]
         action = env.encode_action(action)
         policy_probs[action[0]] = 0.5
         policy_probs[action[1]] = 0.5
-        mcts_history.append([policy_probs])
+        mcts_history.append(policy_probs.tolist())
     del state_history[-1]
     ds.write(info, state_history, mcts_history)
