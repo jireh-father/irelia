e5320277ef67179d47e091293f5146ff22e5175f
seoilgun <seoilgun@gmail.com>
2017-12-04 11:12:43 +0900

    under construction

--- core/model.py
+++ core/model.py
@@ -9,7 +9,7 @@ _BATCH_NORM_EPSILON = 1e-5
 
 class Model(object):
     def __init__(self, sess, input_shape=[10, 9, 17], num_layers=20, num_classes=10 * 9, weight_decay=0.01,
-                 momentum=0.9, use_cache=False):
+                 momentum=0.9, use_cache=False, conf=None):
         self.sess = sess
         self.is_training = tf.placeholder(tf.bool, shape=(), name="is_training")
         self.inputs = None
@@ -18,6 +18,9 @@ class Model(object):
         self.policy_label = None
         self.value_label = None
         self.learning_rate = tf.placeholder(tf.float32, shape=(), name="learning_rate")
+        self.global_step = tf.Variable(0, trainable=False)
+        self.learning_rate = Model.configure_learning_rate(conf.num_steps_per_decay, self.global_step, conf)
+        self.optimizer = Model.configure_optimizer(self.learning_rate, conf)
         self.cost = None
         self.train_op = None
         self.merged = None
@@ -100,7 +103,7 @@ class Model(object):
         # value_loss = tf.losses.mean_squared_error(self.value_label, value_network)
         # policy_loss = tf.losses.softmax_cross_entropy()
 
-        self.train_op = tf.train.MomentumOptimizer(self.learning_rate, self.momentum).minimize(self.cost)
+        self.train_op = self.optimizer.minimize(self.cost)
         self.merged = tf.summary.merge_all()
 
         # todo: accuracy! legal action probabilities and value scalar
@@ -187,3 +190,79 @@ class Model(object):
             action_idx = np.random.choice(len(action_probs), 1, p=action_probs)[0]
         print("choice action idx %d" % action_idx)
         return action_idx
+
+    @staticmethod
+    def configure_optimizer(learning_rate, conf):
+        """Configures the optimizer used for training.
+
+        Args:
+          learning_rate: A scalar or `Tensor` learning rate.
+
+        Returns:
+          An instance of an optimizer.
+
+        Raises:
+          ValueError: if conf.optimizer is not recognized.
+        """
+        if conf.optimizer == 'adadelta':
+            optimizer = tf.train.AdadeltaOptimizer(
+                learning_rate,
+                rho=conf.adadelta_rho,
+                epsilon=conf.opt_epsilon)
+        elif conf.optimizer == 'adagrad':
+            optimizer = tf.train.AdagradOptimizer(
+                learning_rate,
+                initial_accumulator_value=conf.adagrad_initial_accumulator_value)
+        elif conf.optimizer == 'adam':
+            optimizer = tf.train.AdamOptimizer(
+                learning_rate,
+                beta1=conf.adam_beta1,
+                beta2=conf.adam_beta2,
+                epsilon=conf.opt_epsilon)
+        elif conf.optimizer == 'ftrl':
+            optimizer = tf.train.FtrlOptimizer(
+                learning_rate,
+                learning_rate_power=conf.ftrl_learning_rate_power,
+                initial_accumulator_value=conf.ftrl_initial_accumulator_value,
+                l1_regularization_strength=conf.ftrl_l1,
+                l2_regularization_strength=conf.ftrl_l2)
+        elif conf.optimizer == 'momentum':
+            optimizer = tf.train.MomentumOptimizer(
+                learning_rate,
+                momentum=conf.momentum,
+                name='Momentum')
+        elif conf.optimizer == 'rmsprop':
+            optimizer = tf.train.RMSPropOptimizer(
+                learning_rate,
+                decay=conf.rmsprop_decay,
+                momentum=conf.rmsprop_momentum,
+                epsilon=conf.opt_epsilon)
+        elif conf.optimizer == 'sgd':
+            optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+        else:
+            raise ValueError('Optimizer [%s] was not recognized', conf.optimizer)
+        return optimizer
+
+    @staticmethod
+    def configure_learning_rate(decay_steps, global_step, conf):
+
+        if conf.learning_rate_decay_type == 'exponential':
+            return tf.train.exponential_decay(conf.learning_rate,
+                                              global_step,
+                                              decay_steps,
+                                              conf.learning_rate_decay_factor,
+                                              staircase=True,
+                                              name='exponential_decay_learning_rate')
+        elif conf.learning_rate_decay_type == 'fixed':
+            return tf.constant(conf.learning_rate, name='fixed_learning_rate')
+        elif conf.learning_rate_decay_type == 'polynomial':
+            return tf.train.polynomial_decay(conf.learning_rate,
+                                             global_step,
+                                             decay_steps,
+                                             conf.end_learning_rate,
+                                             power=1.0,
+                                             cycle=conf.cycle_learning_rate,
+                                             name='polynomial_decay_learning_rate')
+        else:
+            raise ValueError('learning_rate_decay_type [%s] was not recognized',
+                             conf.learning_rate_decay_type)
--- util/common.py
+++ util/common.py
@@ -66,6 +66,96 @@ def set_flags():
     tf.app.flags.DEFINE_boolean('reuse_mcts', False, "reuse mcts")
     tf.app.flags.DEFINE_boolean('backup_dataset', False, "backup_dataset")
 
+    ######################
+    # Optimization Flags #
+    ######################
+    tf.app.flags.DEFINE_float(
+        'weight_decay', 0.00004, 'The weight decay on the model weights.')
+    tf.app.flags.DEFINE_string(
+        'optimizer', 'rmsprop',
+        'The name of the optimizer, one of "adadelta", "adagrad", "adam",'
+        '"ftrl", "momentum", "sgd" or "rmsprop".')
+
+    tf.app.flags.DEFINE_float(
+        'adadelta_rho', 0.95,
+        'The decay rate for adadelta.')
+
+    tf.app.flags.DEFINE_float(
+        'adagrad_initial_accumulator_value', 0.1,
+        'Starting value for the AdaGrad accumulators.')
+
+    tf.app.flags.DEFINE_float(
+        'adam_beta1', 0.9,
+        'The exponential decay rate for the 1st moment estimates.')
+
+    tf.app.flags.DEFINE_float(
+        'adam_beta2', 0.999,
+        'The exponential decay rate for the 2nd moment estimates.')
+
+    tf.app.flags.DEFINE_float('opt_epsilon', 1.0, 'Epsilon term for the optimizer.')
+
+    tf.app.flags.DEFINE_float('ftrl_learning_rate_power', -0.5,
+                              'The learning rate power.')
+
+    tf.app.flags.DEFINE_float(
+        'ftrl_initial_accumulator_value', 0.1,
+        'Starting value for the FTRL accumulators.')
+
+    tf.app.flags.DEFINE_float(
+        'ftrl_l1', 0.0, 'The FTRL l1 regularization strength.')
+
+    tf.app.flags.DEFINE_float(
+        'ftrl_l2', 0.0, 'The FTRL l2 regularization strength.')
+
+    tf.app.flags.DEFINE_float(
+        'momentum', 0.9,
+        'The momentum for the MomentumOptimizer and RMSPropOptimizer.')
+
+    tf.app.flags.DEFINE_float('rmsprop_momentum', 0.9, 'Momentum.')
+
+    tf.app.flags.DEFINE_float('rmsprop_decay', 0.9, 'Decay term for RMSProp.')
+
+    #######################
+    # Learning Rate Flags #
+    #######################
+
+    tf.app.flags.DEFINE_string(
+        'learning_rate_decay_type',
+        'exponential',
+        'Specifies how the learning rate is decayed. One of "fixed", "exponential",'
+        ' or "polynomial"')
+
+    tf.app.flags.DEFINE_float('learning_rate', 0.01, 'Initial learning rate.')
+
+    tf.app.flags.DEFINE_float(
+        'end_learning_rate', 0.0001,
+        'The minimal end learning rate used by a polynomial decay learning rate.')
+
+    tf.app.flags.DEFINE_float(
+        'label_smoothing', 0.0, 'The amount of label smoothing.')
+
+    tf.app.flags.DEFINE_float(
+        'learning_rate_decay_factor', 0.94, 'Learning rate decay factor.')
+
+    tf.app.flags.DEFINE_float(
+        'num_steps_per_decay', 10000,
+        'Number of epochs after which learning rate decays.')
+
+    tf.app.flags.DEFINE_bool(
+        'sync_replicas', False,
+        'Whether or not to synchronize the replicas during training.')
+
+    tf.app.flags.DEFINE_integer(
+        'replicas_to_aggregate', 1,
+        'The Number of gradients to collect before updating params.')
+
+    tf.app.flags.DEFINE_float(
+        'moving_average_decay', None,
+        'The decay to use for the moving average.'
+        'If left as None, then moving averages are not used.')
+
+    tf.app.flags.DEFINE_boolean('cycle_learning_rate', True, "cycle")
+
 
 def convert_state_history_to_model_input(state_history, num_state_history=7):
     blue_history = []
