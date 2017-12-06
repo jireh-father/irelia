c0a2218f7e17cc9951b88e48cfde8d34388cbf6e
seoilgun <seoilgun@gmail.com>
2017-12-06 22:48:27 +0900

    under construction

--- optimize.sh
+++ optimize.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-nohup python -u optimizer.py --epoch=50 --dataset_dir=/home/data/irelia --model_file_name=dataset_real.csv
+nohup python -u optimizer.py --epoch=50 --dataset_dir=/home/data/irelia --model_file_name=dataset_real.csv > opt.log &
