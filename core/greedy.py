3ab6c55378eb0a3fc4efc6a58ed96e902166b261
seoilgun <seoilgun@gmail.com>
2017-12-10 02:56:03 +0900

    under construction

---
+++ core/greedy.py
@@ -0,0 +1,64 @@
+class Greedy(object):
+    def __init__(self, env):
+        self.env = env
+        self.root_node = None
+        pass
+
+    def search(self, state, action_idx=None):
+
+        if self.root_node is None:
+            i = 0
+            self.root_node = Node(state, self.env.current_turn)
+            stack = [self.root_node]
+            while stack:
+                current_node = stack.pop()
+                if not current_node.extended:
+                    print(i)
+                    i += 1
+                    legal_actions = self.env.get_all_actions(current_node.state)
+                    for legal_action in legal_actions:
+                        next_state, info = self.env.simulate(state, legal_action, True)
+
+                        if info["is_game_over"]:
+                            current_node.update(current_node.turn)
+                        else:
+                            current_node.child_nodes.append(
+                                Node(next_state, 'r' if self.env.current_turn == 'b' else 'b', current_node))
+
+                for child_node in current_node.child_nodes:
+                    stack.append(child_node)
+
+        if action_idx:
+            self.root_node = self.root_node.child_nodes[action_idx]
+
+        max_win = 0
+        max_win_idx = -1
+        for i, child_node in enumerate(self.root_node.child_nodes):
+            if child_node.num_wins > max_win:
+                max_win = child_node.num_wins
+                max_win_idx = i
+        self.root_node = self.root_node.child_nodes[max_win_idx]
+        legal_actions = self.env.get_all_actions(state)
+        return legal_actions[max_win_idx]
+
+
+class Node(object):
+    def __init__(self, state, turn, parent_node=None):
+        self.state = state
+        self.child_nodes = []
+        self.extended = False
+        self.num_wins = 0
+        self.num_loses = 0
+        self.parent_node = parent_node
+        self.turn = turn
+        pass
+
+    def update(self, turn):
+        if turn == self.turn:
+            self.num_wins += 1
+        else:
+            self.num_loses += 1
+
+        if self.parent_node is None:
+            return
+        self.parent_node.update(turn)
---
+++ user_vs_greed_search.py
@@ -0,0 +1,59 @@
+# coding=utf8
+import tensorflow as tf
+from game.game import Game
+from core.greedy import Greedy
+from util import common
+from util import user_input
+import traceback
+import time
+
+FLAGS = tf.app.flags.FLAGS
+
+common.set_flags()
+tf.app.flags.DEFINE_integer('max_rollouts', 20, "exploration step")
+
+env = Game.make("KoreanChess-v1", {"use_check": False, "limit_step": FLAGS.max_step,
+                                   "print_mcts_history": FLAGS.print_mcts_history,
+                                   "use_color_print": FLAGS.use_color_print})
+state = env.reset()
+i = 0
+user_action_idx = -1
+
+
+greedy = Greedy(env)
+action_idx = None
+while True:
+    if i % 2 == 0:
+        from_x, from_y, to_x, to_y = user_input.get_user_input()
+
+        try:
+            legal_actions = env.get_all_actions()
+            user_action = {"from_x": from_x, "from_y": from_y, "to_x": to_x, "to_y": to_y}
+            new_state, reward, done, _ = env.step(user_action)
+
+            for j, legal_action in enumerate(legal_actions):
+                if legal_action == user_action:
+                    user_action_idx = j
+                    break
+            action_idx = user_action_idx
+            if done:
+                print("User win")
+                break
+
+        except Exception as e:
+            print(e)
+            traceback.print_exc()
+            continue
+    else:
+        start_time = time.time()
+        action = greedy.search(state, action_idx)
+        try:
+            new_state, reward, done, info = env.step(action)
+            if done:
+                print("AI win")
+                break
+        except Exception as e:
+            print(e)
+            traceback.print_exc()
+            continue
+    i += 1
