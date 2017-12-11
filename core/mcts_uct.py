cdc32dd913cb5458338815bd66557e0163fd1e71
seoilgun <seoilgun@gmail.com>
2017-12-11 10:30:14 +0900

    under construction

--- core/mcts_uct.py
+++ core/mcts_uct.py
@@ -1,93 +1,108 @@
 from math import sqrt, log
+import numpy as np
+import copy
 
 
 class MctsUct(object):
-    def __init__(self, state, env, max_simulation=500, winner_reward=1., loser_reward=-1., c_puct=2):
+    def __init__(self, env, num_iteration=1500, max_simulation=100, c_puct=2):
         self.env = env
+        self.num_iteration = num_iteration
         self.max_simulation = max_simulation
-        self.root_node = Node(state)
-        self.state_history = [state]
-        self.current_node = self.root_node
-        self.winner_reward = winner_reward
-        self.loser_reward = loser_reward
+        self.root_node = None
+        self.current_node = None
         self.c_puct = c_puct
 
-    def search(self):
-        for i in range(self.max_simulation):
-            self.select()
+    def search(self, state, turn):
+        self.root_node = Node(state, turn)
+        self.current_node = self.root_node
+        for i in range(self.num_iteration):
+            print("iteration %d" % i)
+            selected = True
+            while selected:
+                selected = self.select()
             self.expand()
+            current_node = self.current_node
             value = self.simulation()
-            self.update(value)
-
+            self.current_node = current_node
+            self.current_node.child_nodes = []
+            if value >= 0:
+                self.update(value)
+        return sorted(self.root_node.child_nodes, key=lambda c: c.visits)[-1].action
 
     def select(self):
-        node = node.UCTSelectChild()
-        pass
+        if not self.current_node.child_nodes:
+            return False
+
+        node = self.current_node.select(self.c_puct)
+        self.current_node = node
+        return True
+
+    @staticmethod
+    def get_opponent_turn(turn):
+        return 'b' if turn == 'r' else 'r'
 
     def expand(self):
-        pass
+        legal_actions = self.env.get_all_actions(self.current_node.state)
+        if not legal_actions:
+            return
+        action = legal_actions[np.random.choice(len(legal_actions), 1)[0]]
+        next_state, info = self.env.simulate(self.current_node.state, action)
+        next_node = Node(next_state, MctsUct.get_opponent_turn(self.current_node.turn), self.current_node, action)
+        self.current_node.child_nodes.append(next_node)
+        self.current_node = next_node
 
     def simulation(self):
-        pass
+        is_game_over = False
+        i = 0
+        while not is_game_over:
+            legal_actions = self.env.get_all_actions(self.current_node.state)
+            if not legal_actions:
+                return 0 if self.root_node.turn == self.current_node.turn else 1
+            action = legal_actions[np.random.choice(len(legal_actions), 1)[0]]
+            next_state, info = self.env.simulate(self.current_node.state, action)
+            next_node = Node(next_state, MctsUct.get_opponent_turn(self.current_node.turn),
+                             self.current_node, action)
+            self.current_node.child_nodes.append(next_node)
+            self.current_node = next_node
+            is_game_over = info['is_game_over']
+            i += 1
+            if self.max_simulation <= i:
+                return -1
+        return 0 if self.root_node.turn == self.current_node.turn else 1
 
     def update(self, value):
-        pass
+        node = self.current_node
+        i = 0
+        while node:
+            if node.turn == self.root_node.turn:
+                node.wins += value
+            else:
+                node.wins += -value
+            node.visits += 1.
+            i += 1
+            node = node.parent_node
+        self.current_node = self.root_node
+
 
 class Node:
     """ A node in the game tree. Note wins is always from the viewpoint of playerJustMoved.
         Crashes if state not specified.
     """
 
-    def __init__(self, move=None, parent=None, state=None):
-        self.move = move  # the move that got us to this node - "None" for the root node
-        self.parent_node = parent  # "None" for the root node
+    def __init__(self, state, turn, parent_node=None, action=None):
+        self.action = action
+        self.turn = turn
+        self.parent_node = parent_node
         self.child_nodes = []
-        self.wins = 0
-        self.visits = 0
-        self.untried_moves = state.GetMoves()  # future child nodes
-        self.player_just_moved = state.playerJustMoved  # the only part of the state that the Node needs later
+        self.wins = 0.
+        self.visits = 0.
+        self.state = state
 
-    def select(self):
+    def select(self, c_puct):
         """ Use the UCB1 formula to select a child node. Often a constant UCTK is applied so we have
             lambda c: c.wins/c.visits + UCTK * sqrt(2*log(self.visits)/c.visits to vary the amount of
             exploration versus exploitation.
         """
         s = sorted(self.child_nodes, key=lambda c: c.wins / c.visits + sqrt(2 * log(self.visits) / c.visits))[-1]
-        return s
-
-    def add_child(self, m, s):
-        """ Remove m from untriedMoves and add a new child node for this move.
-            Return the added child node
-        """
-        n = Node(move=m, parent=self, state=s)
-        self.untried_moves.remove(m)
-        self.child_nodes.append(n)
-        return n
-
-    def update(self, result):
-        """ Update this node - one additional visit and result additional wins. result must be from the viewpoint of playerJustmoved.
-        """
-        self.visits += 1
-        self.wins += result
-
-    def __repr__(self):
-        return "[M:" + str(self.move) + " W/V:" + str(self.wins) + "/" + str(self.visits) + " U:" + str(
-            self.untried_moves) + "]"
-
-    def tree_to_string(self, indent):
-        s = self.indent_string(indent) + str(self)
-        for c in self.child_nodes:
-            s += c.TreeToString(indent + 1)
-        return s
-
-    def indent_string(self, indent):
-        s = "\n"
-        for i in range(1, indent + 1):
-            s += "| "
-        return s
-
-    def children_to_string(self):
-        s = ""
-        for c in self.child_nodes:
-            s += str(c) + "\n"
+        # s = sorted(self.child_nodes, key=lambda c: c.wins / c.visits + sqrt(c_puct) * sqrt(self.visits / c.visits))[-1]
         return s
---
+++ self_play_mcts_uct.py
@@ -0,0 +1,33 @@
+from core import play
+import tensorflow as tf
+from util import common
+from util.dataset import Dataset
+import os
+from core.model import Model
+from game.game import Game
+import uuid
+from util.common import log
+from core.mcts_uct import MctsUct
+
+FLAGS = tf.app.flags.FLAGS
+
+common.set_flags()
+
+env = Game.make("KoreanChess-v1",
+                {"use_check": False, "limit_step": FLAGS.max_step, "use_color_print": FLAGS.use_color_print,
+                 "use_cache": FLAGS.use_cache})
+
+mcts = MctsUct(env)
+episode = 0
+state = env.reset()
+while True:
+    """"""
+    """self-play"""
+    log("self-play episode %d" % episode)
+    action = mcts.search(state, env.current_turn)
+
+    state, reward, done, info = env.step(action)
+    if done:
+        break
+
+print("winner", info["winner"])
