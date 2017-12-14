0b2bbc8b415dfcaff971737ef4c8aaf37f98eab8
seoilgun <seoilgun@gmail.com>
2017-12-14 14:12:13 +0900

    under construction

--- parse_dataset.py
+++ parse_dataset.py
@@ -17,6 +17,7 @@ position = {"masangmasang": 0, "masangsangma": 1, "sangmasangma": 2, "sangmamasa
 ds = Dataset()
 # ds.open(os.path.join(FLAGS.dataset_dir, "dataset.csv"))
 ds.open("dataset.csv")
+error = 0
 for n, line in enumerate(f):
     print(n)
     data = json.loads(line)
@@ -33,19 +34,34 @@ for n, line in enumerate(f):
     info = {"over_limit_step": False, "is_draw": False, "winner": winner}
     state, _ = env.encode_state(first_state)
     state_history = [first_state.tolist()]
+    print(actions)
     mcts_history = []
     for i, action in enumerate(actions):
-        turn = "r" if i % 2 == 0 else "b"
-        state[action["to_y"]][action["to_x"]] = state[action["y"]][action["x"]]
-        state[action["y"]][action["x"]] = 0
-        state_history.append(env.decode_state(state, turn).tolist())
-        policy_probs = np.array([.0] * 90)
         action["from_x"] = action["x"]
         action["from_y"] = action["y"]
+        if i > 0:
+            print(action)
+            print(actions[i - 1])
+            if action == actions[i - 1]:
+                error += 1
+                print("same actions with before state")
+                break
+        turn = "r" if i % 2 == 0 else "b"
+
+        state[action["to_y"]][action["to_x"]] = state[action["y"]][action["x"]]
+        state[action["y"]][action["x"]] = 0
+
+        print(i, action)
+        first_state, reward, done, info = env.step(action)
+        state_history.append(env.decode_state(state, turn).tolist())
+        policy_probs = np.array([.0] * 90)
+
         action = env.encode_action(action)
         policy_probs[action[0]] = 0.5
         policy_probs[action[1]] = 0.5
         mcts_history.append(policy_probs.tolist())
-    del state_history[-1]
-    ds.write(info, state_history, mcts_history)
+    # sys.exit()
+    # del state_history[-1]
+    # ds.write(info, state_history, mcts_history)
+print(error)
 ds.close()
