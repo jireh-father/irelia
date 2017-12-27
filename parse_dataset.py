86040b4af85c5c0b72e0f155c76e675c7e7b6311
seoilgun <seoilgun@gmail.com>
2017-12-28 00:19:14 +0900

    under construction

--- parse_dataset.py
+++ parse_dataset.py
@@ -57,11 +57,11 @@ for n, line in enumerate(f):
         policy_probs[action[0]] = 0.5
         policy_probs[action[1]] = 0.5
         mcts_history.append(policy_probs.tolist())
-        break
+        # break
     del state_history[-1]
     if info:
         ds.write(info, state_history, mcts_history)
-    break
+    # break
         # ds.close()
         # sys.exit()
 print(error)
